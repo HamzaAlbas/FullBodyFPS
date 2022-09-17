7c898b4fd949a94c5af75453f1426741ad56ec59
Hamza Albaş <hamza.albas@hotmail.com>
2022-09-17 15:22:40 +0300

    Locked fps to 60

--- FullBodyFPS/Assets/Scripts/MouseLook.cs
+++ FullBodyFPS/Assets/Scripts/MouseLook.cs
@@ -14,6 +14,7 @@ public class MouseLook : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Application.targetFrameRate = 60;
     }
 
     private void Update()
